0e385f25dd5868001fdbddfab994823d43b53239
RodrigOssaV <rjossavillalobos@gmail.com>
2025-01-18 21:59:50 -0300

    agrega información de la empresa

---
+++ src/constants/enterprice.ts
@@ -0,0 +1,30 @@
+export interface Enterprice {
+  name: string;
+  mail: string;
+  phone: string;
+  account?: any;
+
+}
+
+export const enterprice: Enterprice = {
+  name: 'Albania',
+  mail: 'tiendas.albania@gmail.com',
+  phone: '+569 57457027',
+  account: [
+    {
+      id: 1,
+      name: 'TikTok',
+      url: 'https://www.tiktok.com/@albania_chile'
+    },
+    {
+      id: 2,
+      name: 'Instagram',
+      url: 'https://www.instagram.com/albania_chile'
+    },
+    {
+      id: 3,
+      name: 'Facebook',
+      url: 'https://www.facebook.com/profile.php?id=100093969109971'
+    }
+  ]
+}
