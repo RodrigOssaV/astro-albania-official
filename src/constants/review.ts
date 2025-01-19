4687acab78e72b20dbe19f6bee8e39eba1d749e6
RodrigOssaV <rjossavillalobos@gmail.com>
2025-01-19 19:00:37 -0300

    agrega contenido FAQ, estructura FAQ y arreglos menores en estilos

---
+++ src/constants/review.ts
@@ -0,0 +1,11 @@
+export interface Review {
+  id: number;
+  name_person: string;
+  img_profile: string;
+  comment: string;
+  social_media: string;
+}
+
+export const reviews: Review[] = [
+  
+]
