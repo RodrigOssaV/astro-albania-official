31e0b0ad314fe9e7ddeffe3644f302f4a60f5c7a
RodrigOssaV <rjossavillalobos@gmail.com>
2025-01-19 00:53:46 -0300

    agrega iconos a footer social media componente

--- src/constants/enterprice.ts
+++ src/constants/enterprice.ts
@@ -14,17 +14,23 @@ export const enterprice: Enterprice = {
     {
       id: 1,
       name: 'TikTok',
-      url: 'https://www.tiktok.com/@albania_chile'
+      url: 'https://www.tiktok.com/@albania_chile',
+      title: 'Página oficial TikTok de Albania Chile',
+      icon: 'TikTok',
     },
     {
       id: 2,
       name: 'Instagram',
-      url: 'https://www.instagram.com/albania_chile'
+      url: 'https://www.instagram.com/albania_chile',
+      title: 'Página oficial Instagram de Albania Chile',
+      icon: 'Instagram',
     },
     {
       id: 3,
       name: 'Facebook',
-      url: 'https://www.facebook.com/profile.php?id=100093969109971'
+      url: 'https://www.facebook.com/profile.php?id=100093969109971',
+      title: 'Página oficial Facebook de Albania Chile',
+      icon: 'Facebook',
     }
   ]
 }
